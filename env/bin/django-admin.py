0df4b44c65d8c2bf22fa7c558026641bde3c9c7e
solaiman-hridoy <libnum10@gmail.com>
2020-09-19 14:24:19 +0600

    Initial Files

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lib10/Insync/hridoy341@gmail.com/Google Drive/Courses and Projects/My Projects/Hackathon/Hack NSU 2.0/django-dashboard-black/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
