95c868515e3650802c5c3dc8b1c8b7be041ba5a6
tanmoy43 <43274420+tanmoy43@users.noreply.github.com>
2020-09-19 16:09:03 +0600

    updated

--- core/settings.py
+++ core/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
